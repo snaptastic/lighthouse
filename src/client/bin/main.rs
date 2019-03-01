027b15439b92d589ea99b6c29188125ffc281d52
snap <snap@LAPTOP-270SLN39.localdomain>
2019-03-01 15:48:34 -0500

    Making some progress with non-blocking and added in some builtin commands from stdin

--- src/client/bin/main.rs
+++ src/client/bin/main.rs
@@ -1,31 +1,80 @@
 extern crate sodiumoxide;
 
-use std::io::{self, Read, Write};
+use std::io::{self, Read, Write};//, Error, ErrorKind};
 use std::net::TcpStream;
 use sodiumoxide::crypto::kx;
 use sodiumoxide::crypto::aead;
+use sodiumoxide::crypto::aead::Key;
 
-fn main() {
-    let mut buffer = vec![0u8; 65535];
-    sodiumoxide::init();
+use std::{thread, time};
 
-    let (client_pk, client_sk) = kx::gen_keypair();
+#[derive(Debug)]
+struct KeyMaterial {
+    rx: Key,
+    tx: Key,
+    nonce: aead::Nonce,
+}
+
+fn main() -> Result<(), Box<std::error::Error>> {
+    //let buffer = vec![0u8; 65535];
+    sodiumoxide::init().expect("Unable to initialise NACL");
+    let fifteen_secs = time::Duration::new(15, 0);
 
     // Connect to our server, which is hopefully listening.
     let mut connection = TcpStream::connect("127.0.0.1:12345").unwrap();
     println!("connected...");
 
-    send(&mut connection, &client_pk.0);
-    println!("Sending key: {:?}", client_pk.0);
-
-
-    
-    let server_pk = match recv(&mut connection) {
-        Ok(v) => v,
-        Err(e) => {
-            panic!("Error receiving server key");
-        },
+    let client_keys = match key_exchange(&connection){
+        Ok(keys) => keys,
+        Err(error) => match error.kind() {
+            io::ErrorKind::BrokenPipe => {
+                println!("Error: {:?}", error);
+                return Ok(());
+            },
+            error => panic!("Error: {:?}", error),
+        }
     };
+
+    loop {
+        let m = b"Some plaintext";
+        //let ad = b"Some additional data";
+        
+        let c = aead::seal(m, None, &client_keys.nonce, &client_keys.tx);
+
+        match send(&mut connection, &c){
+            Ok(_) => {
+                thread::sleep(fifteen_secs);
+                continue
+            },
+            Err(error) => match error.kind() {
+            io::ErrorKind::BrokenPipe => {
+                println!("Error: {:?}", error);
+                break;
+            },
+            error => panic!("Error: {:?}", error),
+        }
+
+        };
+
+        /*
+        let c = recv(&mut connection).unwrap();
+        let m2 = aead::open(&c, None, &nonce, &rx).unwrap();
+        println!("Message: {}", String::from_utf8(m2).expect("Invalid utf-8"));
+        */
+
+    }
+
+    Ok(())
+}
+
+fn key_exchange(connection: &TcpStream) -> Result<KeyMaterial, io::Error> {
+    //let custom_error = Error::new(ErrorKind::BrokenPipe, "Connection disconnect");
+    let (client_pk, client_sk) = kx::gen_keypair();
+
+    send(&connection, &client_pk.0)?;
+    println!("Sending key: {:?}", client_pk.0);
+    
+    let server_pk = recv(&connection)?;
 
     println!("Receiving server pk: {:?}", server_pk);
     let server_pk = match sodiumoxide::crypto::kx::x25519blake2b::PublicKey::from_slice(&server_pk) {
@@ -38,13 +87,10 @@ fn main() {
     // client deduces the two session keys rx1 and tx1
     let (rx, tx) = match kx::client_session_keys(&client_pk, &client_sk, &server_pk) {
         Ok((rx, tx)) => (rx, tx),
-        Err(()) => panic!("bad server signature"),
+        Err(error) => panic!("bad server signature: {:?}", error),
     };
 
-    println!("Rx {:?}, Tx {:?}", rx, tx);
-
-    //println!("notified server of intent to hack planet.");
-    let nonce = recv(&mut connection).unwrap();
+    let nonce = recv(&connection)?;
     let nonce = match aead::Nonce::from_slice(&nonce){
         Some(v) => v,
         None => {
@@ -53,30 +99,44 @@ fn main() {
     };
 
 
-    let rx = match aead::Key::from_slice(&rx.0) {
+    let key2 = match aead::Key::from_slice(&rx.0) {
         Some(v) => v,
         None => {
             panic!("Failed to convert to aead key");
         }
     };
-    let c = recv(&mut connection).unwrap();
-    let m2 = aead::open(&c, None, &nonce, &rx).unwrap();
-    println!("Message: {}", String::from_utf8(m2).expect("Invalid utf-8"));
+
+    let key1 = match aead::Key::from_slice(&tx.0) {
+        Some(v) => v,
+        None => {
+            panic!("Failed to convert to aead key");
+        }
+    };
+
+    Ok(KeyMaterial {
+        tx: key1,
+        rx: key2,
+        nonce: nonce,
+    })
 }
 
+
 /// Hyper-basic stream transport receiver. 16-bit BE size followed by payload.
-fn recv(stream: &mut TcpStream) -> io::Result<Vec<u8>> {
+fn recv(mut stream: &TcpStream) -> io::Result<Vec<u8>> {
     let mut msg_len_buf = [0u8; 2];
     stream.read_exact(&mut msg_len_buf)?;
     let msg_len = ((msg_len_buf[0] as usize) << 8) + (msg_len_buf[1] as usize);
     let mut msg = vec![0u8; msg_len];
     stream.read_exact(&mut msg[..])?;
+    
     Ok(msg)
 }
 
 /// Hyper-basic stream transport sender. 16-bit BE size followed by payload.
-fn send(stream: &mut TcpStream, buf: &[u8]) {
+fn send(mut stream: &TcpStream, buf: &[u8]) -> io::Result<()> {
     let msg_len_buf = [(buf.len() >> 8) as u8, (buf.len() & 0xff) as u8];
-    stream.write_all(&msg_len_buf).unwrap();
-    stream.write_all(buf).unwrap();
+    stream.write_all(&msg_len_buf)?;
+    stream.write_all(buf)?;
+
+    Ok(())
 }
--- src/server/bin/main.rs
+++ src/server/bin/main.rs
@@ -1,34 +1,288 @@
 extern crate sodiumoxide;
+extern crate mio;
+extern crate rustyline;
 
-use std::io::{self, Read, Write};
-use std::net::TcpStream;
-use std::net::TcpListener;
+use std::io::{self, Read, Write, Error, ErrorKind};
+use std::collections::HashMap;
+use std::net::{IpAddr, Ipv4Addr, SocketAddr, Shutdown};
+use std::time::{Duration};
+use std::{thread};
+use std::os::unix::io::AsRawFd;
+
 use sodiumoxide::crypto::kx;
 use sodiumoxide::crypto::aead;
+use sodiumoxide::crypto::aead::Key;
 
-fn main () {
-    sodiumoxide::init();
+use mio::{Events, Ready, Poll, PollOpt, Token};
+use mio::net::{TcpStream, TcpListener};
+use mio::unix::EventedFd;
+
+use rustyline::error::ReadlineError;
+use rustyline::Editor;
+
+
+#[derive(Debug)]
+struct Client {
+    token: mio::Token,
+    connection: TcpStream,
+    keys: KeyMaterial,
+}
+
+#[derive(Debug)]
+struct KeyMaterial {
+    rx: Key,
+    tx: Key,
+    nonce: aead::Nonce,
+}
+
+fn main () -> Result<(), Box<std::error::Error>> {
+    //let buffer = vec![0u8; 65535];
+    sodiumoxide::init().unwrap();
 
     // Wait on our client's arrival...
     println!("listening on 127.0.0.1:12345");
+    let addr = "127.0.0.1:12345".parse()?;
+    let listener = TcpListener::bind(&addr).unwrap();
+    
+    // The main event loop
+    match socket_event_loop(listener){
+        Ok(_) => {
+            println!("[+] Exiting...");
 
-    let listener = TcpListener::bind("127.0.0.1:12345").unwrap();
-    let (connection, _) = listener.accept().unwrap();
-    println!("Connection Established!");
-    handle_connection(connection);
+            return Ok(())
+        },
+        Err(error) => panic!("Error in event loop: {:?}", error),
+    };
 }
 
-fn handle_connection(mut connection: TcpStream) {
-    let mut buffer = vec![0u8; 65535];
-    let n = aead::gen_nonce();
-    let (server_pk, server_sk) = kx::gen_keypair();
+fn socket_event_loop(listener: TcpListener) -> Result<(), Box<std::error::Error>> {
+    // Pick a token that will not be used by any other socket and use that one
+    // for the listener.
+    const LISTEN_TOKEN: Token = Token(5);
+    const STDIN: Token = Token(0);
 
-    let client_pk = match recv(&mut connection) {
-        Ok(v) => v,
-        Err(e) => {
-            panic!("Error receiving client key");
+    // Used to store the client.
+    let mut sockets = HashMap::new();
+
+    // The `Poll` instance
+    let poll = Poll::new()?;
+
+    let context = "0.0.0.0";
+
+    // Register the listener
+    poll.register(&listener,
+                LISTEN_TOKEN,
+                Ready::readable(),
+                PollOpt::edge())?;
+    
+    // Event storage
+    let mut events = Events::with_capacity(1024);    
+
+    let stdin_fd = io::stdin().as_raw_fd();
+    poll.register(&EventedFd(&stdin_fd),
+             STDIN, Ready::readable(), PollOpt::edge())?;
+    //println!("{:?}", stdin);
+
+    loop {
+        // Wait for events
+        poll.poll(&mut events, None)?;
+
+        for event in &events {
+            //println!("{:?}", event);
+            match event.token() {
+                LISTEN_TOKEN => {
+                    // Perform operations in a loop until `WouldBlock` is
+                    // encountered.
+                    loop {
+                        match listener.accept() {
+                            Ok((mut socket, _)) => {
+                                // Get the token for the socket
+                                let new_client_token: Token = Token(socket.as_raw_fd() as usize);
+
+                                // Register the new socket w/ poll
+                                poll.register(&socket,
+                                            new_client_token,
+                                            Ready::readable(),
+                                            PollOpt::edge())?;
+
+                                let new_keys = match key_exchange(&socket) {
+                                    Ok(keys) => keys,
+                                    Err(error) => {
+                                        println!("{:?}", error);
+                                        break
+                                    },
+                                };
+                                let mut client = Client {
+                                        token: new_client_token,
+                                        connection: socket,
+                                        keys: new_keys
+                                };
+
+                                // Store the socket
+                                sockets.insert(new_client_token, client);
+                            }
+                            Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
+                                // Socket is not ready anymore, stop accepting
+                                break;
+                            }
+                            // Unexpected error
+                            error => panic!("Error: {:?}", error), 
+                        }
+                    }
+                }
+                STDIN => {
+                    match input_loop(&sockets) {
+                        Ok(_) => (),
+                        Err(error) => {
+                            if error == "exit" {
+                                return Ok(());
+                            }
+                        }
+                    };
+                    
+                    print!("{} >> ", context);
+                    io::stdout().flush().unwrap();
+                }
+                token => {
+                    // Always operate in a loop
+                    match match_client(&sockets, token) {
+                        Ok(_) => continue,
+                        Err(error) => match error.kind() {
+                            io::ErrorKind::BrokenPipe => {
+                                /*
+                                let client_ipv4 = match sockets.get(&token){
+                                    Some(client) => client.connection.peer_addr().unwrap(),
+                                    None => SocketAddr::new(IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)), 0),
+                                };
+                                println!("Removing client {:?} and {:?}", client_ipv4, token);
+                                */
+                                sockets.remove(&token);
+                            },
+                            error => panic!("Error: {:?}", error),
+                        },
+                    };
+                }
+            }
+        }
+    }
+}
+
+fn input_loop(sockets: &HashMap<mio::Token, Client>) -> Result<(), &str> {
+    let mut rl = Editor::<()>::new();
+    let builtin_commands = vec![
+        String::from("help"),
+        String::from("hosts"),
+        String::from("quit"),
+    ];
+
+    let readline = rl.readline("");
+    match readline {
+        Ok(line) => {
+            rl.add_history_entry(line.as_ref());
+            match &line[..] {
+                "help" => {
+                    println!("Available commands: {}", builtin_commands.join(", "));
+                },
+                "hosts" => {
+                    if sockets.len() == 0 {
+                        println!("No connected clients");
+                        return Ok(());
+                    }
+                    for (token, client) in sockets {
+                        println!("{}) {}", token.0, client.connection.peer_addr().unwrap());
+                    }
+                },
+                "quit" => {
+                    for (_, client) in sockets {
+                        println!("Closing {}", client.connection.peer_addr().unwrap());
+                        client.connection.shutdown(Shutdown::Both).unwrap();
+                    }
+                    return Err("exit")
+                },
+                _ => {
+                    println!("{}", line);
+                },
+            }
         },
-    };
+        Err(ReadlineError::Interrupted) => {
+            println!("CTRL-C");
+        },
+        Err(ReadlineError::Eof) => {
+            println!("CTRL-D");
+        },
+        Err(err) => {
+            println!("Error: {:?}", err);
+        },
+    }
+
+    Ok(())
+}
+
+/*
+fn accept_client() {
+
+}
+*/
+
+fn match_client(sockets: &HashMap<Token, Client>, token: mio::Token) -> Result<(), io::Error> {
+    let custom_error = Error::new(ErrorKind::BrokenPipe, "Client disconnect");
+    match sockets.get(&token).unwrap() {
+        client => {
+            let _c = match recv(&client.connection) {
+                Ok(c) => c,
+                Err(error) => match error.kind() {
+                    io::ErrorKind::WouldBlock => {
+                        // Socket is not ready anymore, stop reading
+                        return Ok(());
+                        //break;
+                    },
+                    io::ErrorKind::UnexpectedEof => {
+                        //Client disconnect
+                        return Result::Err(custom_error);
+                        //break;
+                    },
+                    // Unexpected error
+                    error => panic!("Error: {:?}", error), 
+                },
+            };
+            //let m2 = aead::open(&c, None, &client.keys.nonce, &client.keys.rx).unwrap();
+            //println!("Message: {}", String::from_utf8(m2).expect("Invalid utf-8"));
+        },
+    }
+
+    Ok(())
+}
+
+fn key_exchange(connection: &TcpStream) -> Result<KeyMaterial, io::Error> {
+    let nonce = aead::gen_nonce();
+    let custom_error = Error::new(ErrorKind::BrokenPipe, "Client disconnect");
+    let (server_pk, server_sk) = kx::gen_keypair();
+    #[allow(unused_assignments)]
+    let mut client_pk = vec![0u8; 65535];
+    let five_seconds = Duration::new(5, 0);
+
+    loop {
+        client_pk = match recv(&connection) {
+            Ok(client_pk) => {
+                client_pk
+            },
+            Err(error) => match error.kind() {
+                io::ErrorKind::WouldBlock => {
+                    // Socket is not ready anymore, stop reading
+                    thread::sleep(five_seconds);
+                    continue;
+                },
+                io::ErrorKind::UnexpectedEof => {
+                    //Client disconnect
+                    return Result::Err(custom_error);
+                    //break;
+                },
+                // Unexpected error
+                error => panic!("Error: {:?}", error), 
+            },
+        };
+        break;
+    }
 
     let client_pk = match sodiumoxide::crypto::kx::x25519blake2b::PublicKey::from_slice(&client_pk) {
         Some(v) => v,
@@ -39,7 +293,7 @@ fn handle_connection(mut connection: TcpStream) {
     println!("Received client public key: {:?}", client_pk);
 
     println!("Sending server public key: {:?}", server_pk.0);
-    send(&mut connection, &server_pk.0);
+    send(&connection, &server_pk.0)?;
 
     // server performs the same operation
     let (rx, tx) = match kx::server_session_keys(&server_pk, &server_sk, &client_pk) {
@@ -47,38 +301,46 @@ fn handle_connection(mut connection: TcpStream) {
         Err(()) => panic!("bad client signature"),
     };
 
-    println!("Rx {:?}, Tx {:?}", rx, tx);
-
-    let nonce = aead::gen_nonce();
-    send(&mut connection, &nonce.0);
-    let m = b"Some plaintext";
-    let ad = b"Some additional data";
-    let tx = match aead::Key::from_slice(&tx.0) {
+    let key1 = match aead::Key::from_slice(&tx.0) {
         Some(v) => v,
         None => {
             panic!("Failed to convert to aead key");
         }
     };
-    let c = aead::seal(m, None, &nonce, &tx);
 
-    send(&mut connection, &c);
+    let key2 = match aead::Key::from_slice(&rx.0) {
+        Some(v) => v,
+        None => {
+            panic!("Failed to convert to aead key");
+        }
+    };
 
-    println!("connection closed.");
+    send(&connection, &nonce.0)?;
+
+    Ok(KeyMaterial {
+        tx: key1,
+        rx: key2,
+        nonce: nonce,
+    })
 }
 
 /// Hyper-basic stream transport receiver. 16-bit BE size followed by payload.
-fn recv(stream: &mut TcpStream) -> io::Result<Vec<u8>> {
+fn recv(mut stream: &TcpStream) -> io::Result<Vec<u8>> {
     let mut msg_len_buf = [0u8; 2];
     stream.read_exact(&mut msg_len_buf)?;
+    //println!("{:?}", msg_len_buf);
     let msg_len = ((msg_len_buf[0] as usize) << 8) + (msg_len_buf[1] as usize);
     let mut msg = vec![0u8; msg_len];
     stream.read_exact(&mut msg[..])?;
+    
     Ok(msg)
 }
 
 /// Hyper-basic stream transport sender. 16-bit BE size followed by payload.
-fn send(stream: &mut TcpStream, buf: &[u8]) {
+fn send(mut stream: &TcpStream, buf: &[u8]) -> io::Result<()> {
     let msg_len_buf = [(buf.len() >> 8) as u8, (buf.len() & 0xff) as u8];
-    stream.write_all(&msg_len_buf).unwrap();
-    stream.write_all(buf).unwrap();
+    stream.write_all(&msg_len_buf)?;
+    stream.write_all(buf)?;
+
+    Ok(())
 }
